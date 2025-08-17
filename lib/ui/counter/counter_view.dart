45b2e5e538aa6a917cb4ae1fd7b94cc41234e0b1
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-08-17 16:57:42 +0500

    stacked state management

--- lib/ui/counter/counter_view.dart
+++ lib/ui/counter/counter_view.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:state_management/ui/counter/counter_viewmodel.dart';
 
 class CounterView extends StatelessWidget {
   const CounterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ViewModelBuilder.reactive(
+      viewModelBuilder :  ()=> CounterViewmodel(),
+       builder: (context, model,child) {
+        return Scaffold(
+          body: Column(
+            children: [
+              Text(model.name),
+              Container(child: Text(model.name),)
+            ],
+          ),
+        );
+       }
+       
+       );
   }
 }
